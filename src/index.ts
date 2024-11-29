c05627d0d75f01c5735d9cb3ba0fa1260fa77878
reasonly8 <reasonly8@sina.com>
2024-11-29 18:06:08 +0800

    feat: add loadProject

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello, World!');
+import { loadProject } from './utils/loadProject';
+import * as path from 'node:path';
+import * as fs from 'node:fs/promises';
+
+const filesMap = await loadProject(path.resolve(__dirname, '../input'));
+
+const prefixPath = path.resolve(__dirname, '../output');
+const dirCheckMap: Record<string, boolean> = {};
+
+for (const [fileRelativePath, content] of Object.entries(filesMap)) {
+  const fileFullPath = path.resolve(prefixPath, fileRelativePath);
+  const fileDir = path.dirname(fileFullPath);
+
+  if (!dirCheckMap[fileDir]) {
+    await fs.mkdir(fileDir, { recursive: true });
+    dirCheckMap[fileDir] = true;
+  }
+
+  fs.writeFile(fileFullPath, content, 'utf-8');
+}
