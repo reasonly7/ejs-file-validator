716d0f34b124da1bfc1fca969a891a480ecff11d
reasonly8 <reasonly8@sina.com>
2024-11-30 16:16:51 +0800

    feat: add diff

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { loadProject } from './utils/loadProject';
 import * as path from 'node:path';
 import * as fs from 'node:fs/promises';
+import { existsSync } from 'node:fs';
 import { ejsContentDiff } from './utils/ejsContentDiff';
 
 const filesMap = await loadProject(path.resolve(__dirname, '../input'));
@@ -8,7 +9,10 @@ const filesMap = await loadProject(path.resolve(__dirname, '../input'));
 const prefixPath = path.resolve(__dirname, '../output');
 const dirCheckMap: Record<string, boolean> = {};
 
-
+if (existsSync(prefixPath)) {
+  await fs.rm(prefixPath, { recursive: true }); // remove output folder
+}
+await fs.mkdir(prefixPath, { recursive: true }); // create output folder
 
 for (const [fileRelativePath, content] of Object.entries(filesMap)) {
   const fileFullPath = path.resolve(prefixPath, fileRelativePath);
--- src/utils/ejsContentDiff.ts
+++ src/utils/ejsContentDiff.ts
@@ -1,3 +1,6 @@
+import * as diff from 'diff';
+
 export const ejsContentDiff = (ejsConent: string, fileContent: string) => {
-  console.log(ejsConent, fileContent);
+  const result = diff.diffChars(fileContent, ejsConent);
+  console.log(result);
 };
